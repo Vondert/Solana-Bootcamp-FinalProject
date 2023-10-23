a99ce10477806cb6a3b7cacd1b67444343474cde
Vondert <25042018avic@gmail.com>
2023-10-23 05:02:33 +0300

    Full project

--- programs/final-project/src/lib.rs
+++ programs/final-project/src/lib.rs
@@ -1,15 +1,163 @@
-use anchor_lang::prelude::*;
-
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{Token, Mint, TokenAccount};
+use anchor_spl::token;
+use anchor_lang::{
+    system_program::*,
+    prelude::*
+};
 declare_id!("8PfXMVu3wzJJnAu1bZxML5aAounkcEQctFbtD4xYS5Mn");
 
 #[program]
 pub mod final_project {
-    use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    use super::*;
+    pub fn create_mint(ctx: Context<CreateMint>, _name: String) -> Result<()> {
+        let mint_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.user_ata.to_account_info(),
+                authority: ctx.accounts.user.to_account_info(),
+            },
+        );
+        msg!("Mint context created");
+        let close_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::SetAuthority{ 
+                current_authority: ctx.accounts.user.to_account_info(), 
+                account_or_mint: ctx.accounts.mint.to_account_info() 
+            }
+        );
+        msg!("Close context created");
+
+        token::mint_to(mint_context, 1)?;
+        msg!("Token minted");
+        token::set_authority(close_context, token::spl_token::instruction::AuthorityType::MintTokens, None)?;
+        msg!("Mint closed {:?}", &ctx.accounts.mint.mint_authority);
+        Ok(())
+   }
+
+   pub fn transfer_token(ctx : Context<Transfer>, _name: String) -> Result<()> {
+        let cpi_context = CpiContext::new(
+ctx.accounts.token_program.to_account_info(),
+token::Transfer {
+                from : ctx.accounts.user_ata.to_account_info(),
+                to : ctx.accounts.recipient_ata.to_account_info(),
+                authority : ctx.accounts.user.to_account_info()
+            },
+        );
+        msg!("Transfer context created");
+        token::transfer(cpi_context, 1)?;
+        msg!("Token transfered");
+        Ok(())
+    }
+
+    pub fn burn_token(ctx : Context<Burn>, _name: String) -> Result<()> {
+        let cpi_context = CpiContext::new(
+ctx.accounts.token_program.to_account_info(),
+   token::Burn {
+                from:ctx.accounts.user_ata.to_account_info(), 
+                authority:ctx.accounts.user.to_account_info(), 
+                mint: ctx.accounts.mint.to_account_info() 
+            },
+        );
+        msg!("Burn context created");
+        token::burn(cpi_context, 1)?;
+        msg!("Token burned");
         Ok(())
     }
 }
 
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String)]
+pub struct CreateMint<'info>{
+    #[account(
+        init,
+         seeds = [
+            b"spl-token-mint".as_ref(),
+            name.as_bytes(),
+         ],
+        bump,
+        payer = user,
+        mint::authority = user,
+        mint::decimals = 0
+    )]
+    pub mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        associated_token::mint = mint,
+        associated_token::authority = user
+    )]
+    pub user_ata: Box<Account<'info, TokenAccount>>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,  
+    pub associated_token_program : Program<'info, AssociatedToken> ,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Transfer<'info>{
+    #[account(
+        seeds = [
+           b"spl-token-mint".as_ref(),
+           name.as_bytes(),
+        ],
+       bump
+   )]
+   pub mint: Account<'info, Mint>,
+
+   #[account(mut)]
+   pub user: Signer<'info>,
+   #[account(mut,
+       associated_token::mint = mint,
+       associated_token::authority = user
+   )]
+   pub user_ata: Box<Account<'info, TokenAccount>>,
+    /// CHECK : We just pass the account info for the demonstration purpose. Ideally this is either signer or trusted account
+   pub recipient: AccountInfo<'info>,
+   #[account(
+    init_if_needed,
+    payer = user,
+    associated_token::mint = mint,
+    associated_token::authority = recipient
+    )]
+   pub recipient_ata: Box<Account<'info, TokenAccount>>,
+
+   pub system_program: Program<'info, System>,
+   pub token_program: Program<'info, Token>,  
+   pub associated_token_program : Program<'info, AssociatedToken> ,
+   pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct Burn<'info>{
+    #[account(
+        mut,
+        seeds = [
+            b"spl-token-mint".as_ref(),
+            name.as_bytes(),
+        ],
+        bump
+    )]
+    pub mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut,
+        associated_token::mint = mint,
+        associated_token::authority = user
+    )]
+    pub user_ata: Box<Account<'info, TokenAccount>>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,  
+    pub associated_token_program : Program<'info, AssociatedToken>,
+}
--- tests/final-project.ts
+++ tests/final-project.ts
@@ -1,16 +1,148 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { FinalProject } from "../target/types/final_project";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, TokenAccountNotFoundError, getMint} from "@solana/spl-token";
+import idl from "../target/idl/final_project.json"; // this generated when we run anchor test command
+import { assert } from "chai";
+import { utf8 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { BN } from "bn.js";
 
 describe("final-project", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  anchor.setProvider(anchor.AnchorProvider.local());
+  const user = anchor.AnchorProvider.local().wallet as anchor.Wallet;
+  const recipient = anchor.web3.Keypair.generate();
   const program = anchor.workspace.FinalProject as Program<FinalProject>;
+  const name = "Vondert";
+  
+  it("First Mint!", async () => {
+    const findNFTMintAddress = async () => {
+      return await PublicKey.findProgramAddressSync(
+        [
+          utf8.encode("spl-token-mint"),
+          utf8.encode(name),
+        ],
+        new PublicKey(idl.metadata.address)
+      );
+    };
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const [mintKey] = (await findNFTMintAddress());
+    const [userAtaKey] = await PublicKey.findProgramAddressSync(
+      [
+        user.publicKey.toBuffer(), 
+        TOKEN_PROGRAM_ID.toBuffer(),
+        mintKey.toBuffer(),
+      ],
+     ASSOCIATED_TOKEN_PROGRAM_ID  
+    );
+
+    const tx = await program.methods.createMint(name).accounts(
+      {
+        mint: mintKey,
+        user: user.publicKey,
+        userAta: userAtaKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      }
+
+    ).signers([user.payer]).rpc();
     console.log("Your transaction signature", tx);
+    const userBalance = (await program.provider.connection.getTokenAccountBalance(userAtaKey)).value.amount;
+    console.log(user.publicKey.toBase58() + " balance " + userBalance);
+    assert(parseInt(userBalance) == 1);
+    let mint = await getMint(program.provider.connection, mintKey);
+    assert(mint.mintAuthority == null);
+  });
+  it("Transfer!", async () => {
+
+    const findNFTMintAddress = async () => {
+      return await PublicKey.findProgramAddress(
+        [
+          utf8.encode("spl-token-mint"),
+          utf8.encode(name),
+        ],
+        new PublicKey(idl.metadata.address)
+      );
+    };
+
+    const [mintKey] = (await findNFTMintAddress());
+    const [userAtaKey] = await PublicKey.findProgramAddress(
+      [
+        user.publicKey.toBuffer(), // could be any public key
+        TOKEN_PROGRAM_ID.toBuffer(),
+        mintKey.toBuffer(),
+      ],
+     ASSOCIATED_TOKEN_PROGRAM_ID  
+    );
+    const [recipientAtaKey] = await PublicKey.findProgramAddress(
+      [
+        recipient.publicKey.toBuffer(), // could be any public key
+        TOKEN_PROGRAM_ID.toBuffer(),
+        mintKey.toBuffer(),
+      ],
+     ASSOCIATED_TOKEN_PROGRAM_ID  
+    );
+
+    const tx = await program.methods.transferToken(name).accounts(
+      {
+        mint: mintKey,
+        user: user.publicKey,
+        userAta: userAtaKey,
+        recipient: recipient.publicKey,
+        recipientAta: recipientAtaKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      }
+
+    ).signers([user.payer]).rpc();
+    console.log("Your transaction signature", tx);
+    const userBalance = (await program.provider.connection.getTokenAccountBalance(userAtaKey)).value.amount;
+    const recipientBalance = (await program.provider.connection.getTokenAccountBalance(recipientAtaKey)).value.amount;
+    console.log(user.publicKey.toBase58() + " balance " + userBalance);
+    console.log(recipient.publicKey.toBase58() + " balance " + recipientBalance);
+    assert(parseInt(userBalance) == 0);
+    assert(parseInt(recipientBalance) == 1);
+  });
+  it("Burn!", async () => {
+
+    const findNFTMintAddress = async () => {
+      return await PublicKey.findProgramAddress(
+        [
+          utf8.encode("spl-token-mint"),
+          utf8.encode(name),
+        ],
+        new PublicKey(idl.metadata.address)
+      );
+    };
+
+    const [mintKey] = (await findNFTMintAddress());
+    const [recipientAtaKey] = await PublicKey.findProgramAddress(
+      [
+        recipient.publicKey.toBuffer(), // could be any public key
+        TOKEN_PROGRAM_ID.toBuffer(),
+        mintKey.toBuffer(),
+      ],
+     ASSOCIATED_TOKEN_PROGRAM_ID  
+    );
+
+    const tx = await program.methods.burnToken(name).accounts(
+      {
+        mint: mintKey,
+        user: recipient.publicKey,
+        userAta: recipientAtaKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      }
+
+    ).signers([recipient]).rpc();
+    console.log("Your transaction signature", tx);
+    const recipientBalance = (await program.provider.connection.getTokenAccountBalance(recipientAtaKey)).value.amount;
+    console.log(recipient.publicKey.toBase58() + " balance " + recipientBalance);
+    assert(parseInt(recipientBalance) == 0);
+
   });
 });
